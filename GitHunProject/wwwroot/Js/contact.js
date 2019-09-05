2f399f23316978927f4008bb92e44ebde23009fb
Mohamed Yussuf <myussuf03@gmail.com>
2019-09-04 22:14:27 -0400

    HTML

---
+++ GitHunProject/wwwroot/Js/contact.js
@@ -0,0 +1,35 @@
+﻿
+
+$(document).ready(function () {
+
+    //create yiur local veriables
+    
+    var fName = null;
+    var lName = null;
+    var country = null;
+    var subject = null;
+
+   // get form data and save it into local variable
+
+    
+    
+    $('#btnSubmit').click(function () {
+
+        fName = $('#firstname').val();
+        lName = $('#lastname').val();
+        country = $('#country').val();
+        subject = $('#subject').val();
+
+        $('#fName').text('First Name: '+fName);
+        $('#lName').text('Last Name: '+lName);
+        $('#cntry').text('contry: ' +country);
+        $('#sbjt').text('subject:' + subject);
+
+
+        $('#divForm').hide();
+
+
+    })
+
+    //alert the saved data
+});
